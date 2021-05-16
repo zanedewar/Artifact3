fe9aacd18031b041699a36355effef48a91a8bf0
zanedewar <zane_dewar@hotmail.com>
2021-05-15 17:55:47 -0700

    CSS

--- Artifact3js.js
+++ Artifact3js.js
@@ -4,49 +4,47 @@
 // var symbol ="*";
 
 function createRhombus(pHeight, pColorEven, pColorOdd, pSymbol) {
-upRight(pHeight, pColorEven, pColorOdd, pSymbol);
-downRight(pHeight, pColorEven, pColorOdd, pSymbol);
+	upRight(pHeight, pColorEven, pColorOdd, pSymbol);
+	downRight(pHeight, pColorEven, pColorOdd, pSymbol);
 
 }
 
 function upRight(pHeight, pColorEven, pColorOdd, pSymbol){
-var rLine ="";
-for (i=0;i<pHeight;i++){
-rLine +="<p>";
-//Create each line on the Rhombus
-for(j=0;j<=i;j++){
+	var rLine ="";
+	for (i=0;i<pHeight;i++){
+		rLine +="<p>";
+		//Create each line on the Rhombus
+		for(j=0;j<=i;j++){
+			//Is the position even or odd so we change the color
+			if (j%2)
+			//even
+			rLine +="<span style='color:" + pColorEven + ";'>" + pSymbol +"</span>";
+			else
+			//odd
+			rLine +="<span style='color:" + pColorOdd + ";'>" + pSymbol +"</span>";
+		}
+		rLine +="</p>";
+		// console.log(rLine);
 
-//Is the position even or odd so we change the color
-if (j%2)
-//even
-rLine +="<span style='color:" + pColorEven + ";'>" + pSymbol +"</span>";
-else
-//odd
-rLine +="<span style='color:" + pColorOdd + ";'>" + pSymbol +"</span>";
+	}
 
-}
-rLine +="</p>";
-// console.log(rLine);
-
-}
-
-document.getElementById("upRight").innerHTML = rLine;
+	document.getElementById("upRight").innerHTML = rLine;
 }
 
 function downRight(pHeight, pColorEven, pColorOdd, pSymbol){
-var rLine ="";
-for (i=pHeight;i > 0;i--){
-rLine +="<p>";
-//Create each line on the Rhombus
-for(j=0;j<i;j++){
+	var rLine ="";
+	for (i=pHeight;i > 0;i--){
+	rLine +="<p>";
+	//Create each line on the Rhombus
+	for(j=0;j<i;j++){
 
-//Is the position even or odd so we change the color
-if (j%2)
-//even
-rLine +="<span style='color:" + pColorEven + ";'>" + pSymbol +"</span>";
-else
-//odd
-rLine +="<span style='color:" + pColorOdd + ";'>" + pSymbol +"</span>";
+	//Is the position even or odd so we change the color
+	if (j%2)
+	//even
+	rLine +="<span style='color:" + pColorEven + ";'>" + pSymbol +"</span>";
+	else
+	//odd
+	rLine +="<span style='color:" + pColorOdd + ";'>" + pSymbol +"</span>";
 
 }
 rLine +="</p>";
@@ -55,4 +53,7 @@ rLine +="</p>";
 }
 
 document.getElementById("downRight").innerHTML = rLine;
+}
+function upLeft(pHeight, pColorEven, pColorOdd, pSymbol) {
+	
 }
