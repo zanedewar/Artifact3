20fd51e7d115d39f49584ca98f3d0cf169a7ca9f
zanedewar <zane_dewar@hotmail.com>
2021-05-15 18:11:16 -0700

    downLeft

--- Artifact3js.js
+++ Artifact3js.js
@@ -77,3 +77,23 @@ function upLeft(pHeight, pColorEven, pColorOdd, pSymbol) {
 
 	document.getElementById("upLeft").innerHTML = rLine;
 }
+function downLeft (pHeight, pColorEven, pColorOdd, pSymbol){
+	var rLine ="";
+	for (i=pHeight;i > 0;i--){
+	rLine +="<p>";
+	//Create each line on the Rhombus
+	for(j=0;j<i;j++){
+
+	//Is the position even or odd so we change the color
+	if (j%2)
+	//even
+	rLine +="<span style='color:" + pColorEven + ";'>" + pSymbol +"</span>";
+	else
+	//odd
+	rLine +="<span style='color:" + pColorOdd + ";'>" + pSymbol +"</span>";
+
+}
+rLine +="</p>";
+// console.log(rLine);
+	document.getElementById("downLeft").innerHTML = rLine;
+}
